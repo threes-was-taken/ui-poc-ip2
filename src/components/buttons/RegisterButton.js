31d088880209165cc9cbaea003a87861582e5308
Joren Koyen <koyenjoren@gmail.com>
2020-11-28 19:04:54 +0100

    Replace Button component with default html tag 'button'

--- src/components/buttons/RegisterButton.js
+++ src/components/buttons/RegisterButton.js
@@ -1,12 +1,12 @@
-import React, { Button, Component } from 'react'
-import '../../css/HomeButtons.css'
+import React, { Component } from "react";
+import "../../css/HomeButtons.css";
 
 export default class RegisterButton extends Component {
   render() {
     return (
       <div>
-        <Button id="RegisterButton">Register</Button>
+        <button id="RegisterButton">Register</button>
       </div>
-    )
+    );
   }
 }
--- src/components/buttons/TutorialButton.js
+++ src/components/buttons/TutorialButton.js
@@ -1,12 +1,12 @@
-import React, { Button, Component } from 'react'
-import '../../css/HomeButtons.css'
+import React, { Component } from "react";
+import "../../css/HomeButtons.css";
 
 export default class TutorialButton extends Component {
   render() {
     return (
       <div>
-        <Button id="TutorialButton">Tutorial</Button>
+        <button id="TutorialButton">Tutorial</button>
       </div>
-    )
+    );
   }
 }
